14dd6c5fd006a2662ad43e98706dc8783a6ca600
Gabriel <gabrielivansaliba@gmail.com>
2020-06-15 17:05:46 -0300

    descargar excel prueba ok

--- src/app/componentes/descargas/descargas.component.ts
+++ src/app/componentes/descargas/descargas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExcelService } from '../../servicios/excel.service';
 
 @Component({
   selector: 'app-descargas',
@@ -9,10 +10,28 @@ export class DescargasComponent implements OnInit {
 
   especialidadSeleccionada: string = "";
   diaSeleccionado: string = "";
-  
-  constructor() { }
+
+  data: any = [{
+    eid: 'e101',
+    ename: 'ravi',
+    esal: 1000
+  }, {
+    eid: 'e102',
+    ename: 'ram',
+    esal: 2000
+  }, {
+    eid: 'e103',
+    ename: 'rajesh',
+    esal: 3000
+  }];
+
+  constructor(private excelService: ExcelService) { }
 
   ngOnInit(): void {
+  }
+
+  exportarComoExcel(): void {
+    this.excelService.exportAsExcelFile(this.data, 'sample');
   }
 
 }
